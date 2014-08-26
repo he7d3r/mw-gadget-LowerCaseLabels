7d99d0fb0e13a9672eb13752d95a638d878fcedf
Helder <he7d3r@gmail.com>
2014-08-26 15:44:23 -0300

    Rename the script

--- src/LowerCaseLabels.js
+++ src/LowerCaseLabels.js
@@ -1,8 +1,8 @@
 /**
- * Adds links to fix the capitalization of labels on Wikidata 
+ * Adds links to fix the capitalization of labels on Wikidata
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Capitalization.js]] ([[File:User:Helder.wiki/Tools/Capitalization.js]])
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/LowerCaseLabels.js]] ([[File:User:Helder.wiki/Tools/LowerCaseLabels.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
