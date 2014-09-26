27c979cfd17536095994d3281ae47b651c364836
Helder <he7d3r@gmail.com>
2014-09-26 15:21:25 -0300

    Define JSHint configuration on .jshintrc

--- src/LowerCaseLabels.js
+++ src/LowerCaseLabels.js
@@ -3,15 +3,13 @@
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
-/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
-/*global jQuery, mediaWiki */
 ( function ( mw, $ ) {
 'use strict';
 
 	var i18nData = {
 		'en': {
 			'link-lower-case-first': 'LCFirst',
-			'link-lower-case-first-description': 'Converts the first character to lower case',
+			'link-lower-case-first-description': 'Converts the first character to lower case'
 		}
 	};
 	mw.messages.set( $.extend( i18nData.en, i18nData[ mw.config.get( 'wgUserLanguage' ) ] ) );
